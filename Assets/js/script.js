8784f7a96e0ec0bbb6fd44a6ecb1d59346c25c9a
johngeorge88 <johniskandar@hotmail.com>
2020-09-13 14:36:16 -0400

    fixing appearce

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -6,7 +6,7 @@ var searchHistory = document.getElementById("search-history");
 
 
 var searchHandler = function (cityName) {
-    event.preventDefault();
+    cityName.preventDefault();
     currentDayCard.innerHTML = "";
     forecastData.innerHTML = "";
     var cityName = searchInput.value.trim();
@@ -22,12 +22,18 @@ var searchHandler = function (cityName) {
 
 var cityHistory = function (city) {
     var historyEl = document.createElement('Button');
-    historyEl.setAttribute("class", "list-group-item list-group-item-action")
+    historyEl.setAttribute("class", "city-item btn list-group-item list-group-item-action");
+    historyEl.setAttribute("id", city)
     historyEl.textContent = city;
     searchHistory.append(historyEl);
 
+    historyEl.onclick = clickCity;
 }
 
+var clickCity = function() {
+    var cityName = this.id;
+    getWeatherInfo(cityName);
+}
 
 var getWeatherInfo = function (cityName) {
     var currentURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=42ea263982f3e6fdce056c448e41a364" + "&units=imperial";
@@ -39,12 +45,23 @@ var getWeatherInfo = function (cityName) {
             if (response.ok) {
                 response.json().then(function (data) {
                     console.log(data);
-                    displayTodayWeather(data);
+                    var lat = data.coord.lat;
+                    var lon = data.coord.lon;
+
+                    return fetch("https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=42ea263982f3e6fdce056c448e41a364")
+                        .then(function (UVresponse) {
+                            UVresponse.json().then(function (UVdata) {
+                                console.log(UVdata)
+
+                                displayTodayWeather(data, UVdata);
+                            })
+                        });
                 });
-            } else {
-                alert("Error: " + response.statusText);
+                } else {
+                    alert("Error: " + response.statusText);
+                    
             }
-        })
+        });
     fetch(forecastURL).then(function (response) {
         if (response.ok) {
             response.json().then(function (forecast) {
@@ -56,7 +73,9 @@ var getWeatherInfo = function (cityName) {
 
 };
 
-var displayTodayWeather = function (data) {
+var displayTodayWeather = function (data, UVdata) {
+    currentDayCard.innerHTML= "";
+    currentDayCard.setAttribute("class", "card");
     // Current Day
     var todayCad = document.createElement("div");
     currentDayCard.appendChild(todayCad);
@@ -73,13 +92,20 @@ var displayTodayWeather = function (data) {
     var todayWindSpeed = document.createElement("p");
     todayWindSpeed.textContent = "Wind Speed: " + data.wind.speed + " MPH";
     currentDayCard.appendChild(todayWindSpeed);
-    var todayUVindex = document.createElement("p");
-    todayUVindex.textContent = "UV Index: " + "<span class='alert alert-danger'>" + data.wind.speed + "</span>";
 
+    var todayUVindex = document.createElement("p");
+    todayUVindex.textContent = "UV Index:";
+    var UVindex = document.createElement("span");
+    UVindex.setAttribute("class", "badge badge-danger")
+    UVindex.textContent = UVdata.value;
     currentDayCard.appendChild(todayUVindex);
+    todayUVindex.appendChild(UVindex);
 }
 
 var displayForecastWeather = function (forecast) {
+    var forecastWeather = document.getElementById("Forecast-data");
+    forecastWeather.innerHTML="";
+
     var forecastEl = document.createElement("div");
     forecastData.appendChild(forecastEl);
 
@@ -89,38 +115,38 @@ var displayForecastWeather = function (forecast) {
 
 
     // Create 5 cards
-    for (var i = 0; i < forecast.list.length; i++) {
-        if (i <= 4) {
-            var cardHolder = document.createElement("div")
-            cardHolder.setAttribute("class", "card-deck d-inline-block text-white bg-primary mr-3 mb-3")
-            cardHolder.style = "max-width: 10rem;";
-            forecastData.appendChild(cardHolder);
+    for (var i = 0; i < forecast.list.length; i += 8) {
+        // if (i <= 4) {
+        var cardHolder = document.createElement("div")
+        cardHolder.setAttribute("class", "card-deck d-inline-block text-white bg-primary cardInfo")
+        // cardHolder.style = "max-width: 10rem;";
+        forecastData.appendChild(cardHolder);
 
-            var cardDate = document.createElement("div")
-            cardDate.setAttribute("class", "card-header")
-            cardDate.textContent = forecast.list[i].dt_txt;
-            cardHolder.appendChild(cardDate)
+        var cardDate = document.createElement("div")
+        cardDate.setAttribute("class", "card-header")
+        var dateTime = forecast.list[i].dt_txt.split(" ")
+        var formatDate = moment(dateTime[0]).format("MM/DD/YY")
+        cardDate.textContent = formatDate;
+        cardHolder.appendChild(cardDate)
 
-            var cardBody = document.createElement("div")
-            cardBody.setAttribute("class", "card-body")
-            cardDate.appendChild(cardBody)
+        var cardBody = document.createElement("div")
+        cardBody.setAttribute("class", "card-body")
+        cardDate.appendChild(cardBody)
 
-            var weatherIcon = document.createElement("img")
-            weatherIcon.setAttribute("src", "https://openweathermap.org/img/wn/" + forecast.list[i].weather[0].icon + "@2x.png");
-            cardBody.appendChild(weatherIcon);
+        var weatherIcon = document.createElement("img")
+        weatherIcon.setAttribute("src", "https://openweathermap.org/img/wn/" + forecast.list[i].weather[0].icon + "@2x.png");
+        cardBody.appendChild(weatherIcon);
 
-            var cardTemp = document.createElement("p");
-            cardTemp.textContent = "Temperature: " + forecast.list[i].main.temp + " °F";
-            cardTemp.setAttribute("class", "card-text");
-            cardBody.appendChild(cardTemp);
-            
-            var cardHumidity = document.createElement("p");
-            cardHumidity.textContent = "Humidity: " + forecast.list[i].main.humidity + "%";
-            cardHumidity.setAttribute("class", "card-text");
-            cardBody.appendChild(cardHumidity);
+        var cardTemp = document.createElement("p");
+        cardTemp.textContent = "Temp: " + forecast.list[i].main.temp + " °F";
+        cardTemp.setAttribute("class", "card-text");
+        cardBody.appendChild(cardTemp);
 
-
-        }
+        var cardHumidity = document.createElement("p");
+        cardHumidity.textContent = "Humidity: " + forecast.list[i].main.humidity + "%";
+        cardHumidity.setAttribute("class", "card-text");
+        cardBody.appendChild(cardHumidity);
+        // }
     }
 
 }
